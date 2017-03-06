401ef9cdc43d2fcf7ff2c2c7b000b50f66b0798d
msipiora <sipiora@uwalumni.com>
2017-03-06 10:41:55 -0600

    Final

---
+++ CustomList/CustomList/CustomList.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+
+        T[] array;
+        public int capacity = 5;
+        public int size;
+        private int counter;
+        public int Count { get { return counter; } }
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+
+        public CustomList()
+        {
+            array = new T[0];
+
+        }
+
+
+
+        public void Add(T value)
+        {
+            T[] tempArray = new T[array.Length + 1];
+            counter++;
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                tempArray[index] = array[index];
+            }
+
+            tempArray[tempArray.Length - 1] = value;
+
+            array = tempArray;
+
+        }
+
+
+        public bool Remove(T value)
+        {
+            for (int i = 0; i < counter; i++)
+            {
+                if (array[i].Equals(value))
+                {
+                    int indexFirstInstance = i;
+
+                    for (int j = indexFirstInstance; j < counter-1; j++)
+                    {
+                        array[j] = array[j + 1];
+                       
+                       
+                    }
+                    counter--;
+                    return true;
+                } 
+               
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach (T value in array)
+            {
+                result += value.ToString();
+            }
+            return result;
+        }
+
+        public static CustomList<T> operator +(CustomList<T> listOne, CustomList<T> listTwo)
+        {
+            CustomList<T> combinedArray = new CustomList<T>();
+
+            for (int i = 0; i < listOne.Count; i++)
+            {
+                combinedArray.Add(listOne.array[i]);
+            }
+
+            for (int i = 0; i < listTwo.Count; i++)
+            {
+                combinedArray.Add(listTwo.array[i]);
+            }
+
+            return combinedArray;
+
+
+
+        }
+
+        public static CustomList<T> operator -(CustomList<T> listOne, CustomList<T> listTwo)
+        {
+            for (int i = 0; i < listOne.Count; i++)
+            {
+                for (int j = 0; j < listTwo.Count; j++)
+                {
+                    if (listOne.array[i].Equals(listTwo.array[j]))
+                    {
+                        listOne.Remove(listTwo.array[j]);
+                    }
+                }
+            }
+            return listOne;
+        }
+
+        public CustomList<T> ZipList(CustomList<T> listOne)
+        {
+            CustomList<T> ZipList = new CustomList<T>();
+
+            for (int i = 0; i < listOne.Count * 2; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    ZipList.Add(listOne[i / 2]);
+                }
+                else
+                    ZipList.Add(this[i / 2]);
+            }
+            return ZipList;
+        }
+
+        //public IEnumerator : GetEnumerator()
+        //{
+        //    //this is a reason to use var as the data type for the temporary variable
+        //    //in a foreach loop
+        //    yield return custom;
+
+        //    for (int i = 0; i < custom.Count(); i++)
+        //    {
+        //        yield return custom[i];
+        //    }
+        //}
+    }
+    }
+
+
---
+++ CustomList/CustomListTest/CustomListTest.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class CustomListTest
+    {
+
+        public string custom3;
+
+        [TestMethod]
+        public void AddOneObject()
+        {
+
+
+            CustomList<int> custom = new CustomList<int>();
+
+            //Act
+            custom.Add(6);
+            //Assert
+            Assert.AreEqual(custom[0], 6);
+        }
+
+        [TestMethod]
+        public void AddMultipleObjects()
+        {
+
+
+            CustomList<int> custom = new CustomList<int>();
+
+            //Act
+            custom.Add(6);
+            custom.Add(12);
+            //Assert
+            Assert.AreEqual(custom[1], 12);
+        }
+
+        [TestMethod]
+        public void TestArrayExpansion()
+        {
+
+            //Arrange
+
+            CustomList<int> custom = new CustomList<int>();
+
+            //Act
+            custom.Add(1);
+            custom.Add(2);
+            custom.Add(3);
+            custom.Add(4);
+            custom.Add(5);
+            custom.Add(6);
+            //Assert
+            Assert.AreEqual(6, custom.Count);
+        }
+        [TestMethod]
+
+        public void CheckObjectPosition()
+        {
+            //Arrange
+            CustomList<int> custom = new CustomList<int>();
+
+            //Act
+            custom.Add(6);
+            custom.Add(12);
+            //Assert
+            Assert.AreEqual(custom[1], 12);
+        }
+
+
+        //If remove succeeds, decrease the count by 1.
+        //If remove fails, keep count.
+        //Do this inside Ienumerator Get GetEnumerator()for loop inside for loop data array [i]
+
+        [TestMethod]
+        public void RemoveOneObject()
+        {
+
+            //Arrang
+            CustomList<int> custom = new CustomList<int>();
+            custom.Add(6);
+            custom.Add(1);
+            //Act
+            custom.Remove(6);
+            //Assert
+            Assert.AreEqual(1, custom.Count);
+
+        }
+        [TestMethod]
+
+        public void RemoveMultipleObjects()
+        {
+            //Arrang
+
+            CustomList<int> custom = new CustomList<int>();
+
+            //Act
+            custom.Add(6);
+            custom.Add(12);
+            custom.Remove(6);
+            custom.Remove(12);
+            //Assert
+            Assert.AreEqual(custom.Count, 0);
+        }
+
+        [TestMethod]
+
+        public void RemoveMultipleObjects2()
+        {
+            //Arrang
+
+            CustomList<int> custom = new CustomList<int>();
+
+            //Act
+            custom.Add(6);
+            custom.Add(12);
+            custom.Add(18);
+            custom.Remove(6);
+            custom.Remove(12);
+            custom.Remove(18);
+            //Assert
+            Assert.AreEqual(custom[0], 18);
+        }
+
+
+        [TestMethod]
+        public void CounterChecker()
+        {
+
+            //Arrange
+
+            CustomList<int> custom = new CustomList<int>();
+
+
+            //Act
+
+            custom.Add(1);
+            custom.Add(2);
+
+            //Assert
+            Assert.AreEqual(2, custom.Count);
+
+        }
+
+        [TestMethod]
+        public void CounterChecker2()
+        {
+
+            //Arrange
+
+            CustomList<int> custom = new CustomList<int>();
+
+
+            //Act
+
+            custom.Add(1);
+            custom.Add(2);
+            custom.Add(18);
+            custom.Add(6);
+
+            //Assert
+            Assert.AreEqual(4, custom.Count);
+
+        }
+
+        [TestMethod]
+        public void CounterChecker3()
+        {
+
+            //Arrange
+
+            CustomList<int> custom = new CustomList<int>();
+
+
+            //Act
+
+            custom.Add(1);
+            custom.Add(2);
+            custom.Add(3);
+            custom.Add(4);
+            custom.Add(20000);
+            custom.Remove(1);
+
+
+            //Assert
+            Assert.AreEqual(custom[3], 20000);
+
+        }
+
+        [TestMethod]
+        public void CheckIndexAfterRemoval()
+        {
+            CustomList<int> custom = new CustomList<int>();
+
+            //Arrang
+            custom.Add(6);
+            custom.Add(17);
+            custom.Add(12);
+            //Act
+            custom.Remove(17);
+
+            //Assert
+            Assert.AreEqual(12, custom[1]);
+        }
+
+        [TestMethod]
+        public void CheckStringOverride()
+        {
+
+            //Arrange
+
+            CustomList<int> custom = new CustomList<int>();
+            custom.Add(3);
+            custom.Add(4);
+            custom.Add(6);
+
+            string expected = "346";
+            //Act
+            string result = custom.ToString();
+            //Assert
+            Assert.AreEqual(result, expected);
+
+        }
+
+
+
+        [TestMethod]
+
+        public void OverridePlus()
+        {
+
+            //Arrange
+
+            CustomList<int> custom = new CustomList<int>();
+            CustomList<int> custom2 = new CustomList<int>();
+            CustomList<int> custom3 = new CustomList<int>();
+            //Act
+
+            custom.Add(1);
+            custom2.Add(2);
+            custom3 = custom + custom2;
+
+            //Assert
+            Assert.AreEqual(custom3[0], 1);
+
+        }
+
+        [TestMethod]
+        public void CheckMinusOverride()
+        {
+
+            //Arrange
+
+            CustomList<int> custom = new CustomList<int>();
+            CustomList<int> custom2 = new CustomList<int>();
+            CustomList<int> custom3 = new CustomList<int>();
+
+            //Act
+
+            custom.Add(1);
+            custom.Add(2);
+            custom2.Add(1);
+            custom3 = custom - custom2;
+
+
+
+            //Assert
+            Assert.AreEqual(custom3[0], 2);
+
+        }
+
+        [TestMethod]
+        public void CheckMinusOverride2()
+        {
+
+            //Arrange
+
+            CustomList<int> custom = new CustomList<int>();
+            CustomList<int> custom2 = new CustomList<int>();
+            CustomList<int> custom3 = new CustomList<int>();
+
+
+            //Act
+
+            custom.Add(1);
+            custom.Add(2);
+            custom.Add(3);
+            custom.Add(4);
+            custom2.Add(1);
+            custom2.Add(500);
+            custom2.Add(3);
+            custom2.Add(4);
+            custom3 = custom - custom2;
+
+
+
+            //Assert
+            Assert.AreEqual(custom3[1], 4);
+
+        }
+
+        [TestMethod]
+        public void CheckMinusOverride3()
+        {
+
+            //Arrange
+
+            CustomList<int> custom = new CustomList<int>();
+            CustomList<int> custom2 = new CustomList<int>();
+            CustomList<int> custom3 = new CustomList<int>();
+
+
+            //Act
+
+            custom.Add(1);
+            custom.Add(2);
+            custom.Add(2);
+            custom2.Add(2);
+            custom3 = custom - custom2;
+
+
+
+            //Assert
+            Assert.AreEqual(custom3[1], 2);
+
+        }
+
+
+        [TestMethod]
+
+        public void ZipTestInt()
+        {
+
+            //Arrange
+            CustomList<int> even = new CustomList<int>();
+            CustomList<int> odd = new CustomList<int>();
+            CustomList<int> result = new CustomList<int>();
+
+            //Act
+
+            odd.Add(1);
+            odd.Add(3);
+            odd.Add(5);
+            even.Add(2);
+            even.Add(4);
+            even.Add(6);
+
+            result = even.ZipList(odd);
+
+            //Assert
+            Assert.AreEqual(4, result[3]);
+
+        }
+
+
+        [TestMethod]
+        public void ZipTestString()
+        {
+
+            //Arrange
+            CustomList<string> even = new CustomList<string>();
+            CustomList<string> odd = new CustomList<string>();
+            CustomList<string> result = new CustomList<string>();
+
+
+            //Act
+
+            odd.Add("First");
+            odd.Add("Third");
+            even.Add("Second");
+            even.Add("Fourth");
+
+            result = even.ZipList(odd);
+            //Assert
+            Assert.AreEqual(result.ToString(), "FirstSecondThirdFourth");
+        }
+
+        //    [TestMethod]
+        //    public void TestCustomIterate()
+        //    {
+
+        //        //Arrange
+        //        CustomList<string> custom = new CustomList<string>();
+
+
+
+        //        //Act
+
+        //        custom.Add("1234");
+        //        custom.Add("5678");
+        //        custom.Add("910 ");
+        //        custom.Add("Testing is worse than being waterboarded");
+
+        //        foreach (string value in custom)
+        //        {
+        //            //result.Add(value)
+        //        }
+        //        //Assert
+        //        Assert.AreEqual(result, "First Second Third Fourth");
+        //    }
+        //} }
+
+
+
+
+
+
+    }
+    }
